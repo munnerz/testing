015565b8f55d45ec9b1952b64caa6b91bbdbfae9
James Munnelly <james@munnelly.eu>
2019-04-09 00:52:29 +0100

    Update ci-runner.sh to bump job configs
    Signed-off-by: James Munnelly <james@munnelly.eu>

--- images/builder/ci-runner.sh
+++ images/builder/ci-runner.sh
@@ -27,15 +27,61 @@ shift
 
 WORKSPACE="$(bazel info workspace)"
 
-echo "Activating service account..."
-gcloud auth activate-service-account --key-file="${GOOGLE_APPLICATION_CREDENTIALS}"
+# echo "Activating service account..."
+# gcloud auth activate-service-account --key-file="${GOOGLE_APPLICATION_CREDENTIALS}"
 
-echo "Generating docker credentials..."
-gcloud auth configure-docker --quiet
+# echo "Generating docker credentials..."
+# gcloud auth configure-docker --quiet
 
 echo "Executing builder..."
-bazel run \
+PUSHED_IMAGE=$(bazel run \
     //images/builder -- \
-    --build-dir "${WORKSPACE}"/"${BUILD_DIR}" "$@"
+    --build-dir "${WORKSPACE}"/"${BUILD_DIR}" "$@")
 
 echo "Build complete!"
+
+if [ -z "${PUSHED_IMAGE}" ]; then
+    echo "No image pushed to registry"
+    exit 0
+fi
+
+echo "Pushed image ${PUSHED_IMAGE}"
+echo
+
+user="${GITHUB_USER:-}"
+token="${GITHUB_TOKEN_FILE:-}"
+if [ -z "${user}" ] || [ -z "${token}" ]; then
+    echo "Skipping patching job configs"
+    exit 0
+fi
+
+echo "Patching YAML files for new image"
+find "${WORKSPACE}/config/jobs" -type f -name '*.yaml' | \
+    xargs bazel run //prow/image-bumper -- \
+    --image-regex "${PUSHED_IMAGE}"
+
+ensure-config() {
+  git config user.name &>/dev/null && git config user.email &>/dev/null && return 0
+  local username="jetstack-bot"
+  local email="jetstack-bot@users.noreply.github.com"
+  echo "git config user.name=$username user.email=$email..." >&2
+  git config user.name "$username"
+  git config user.email "$email"
+}
+ensure-config "$@"
+
+title="Bump ${BUILD_DIR} jobs"
+
+git add -A
+git commit -m "${title}"
+git push -f "git@github.com:${user}/testing.git" HEAD:autobump-"${BUILD_DIR}"
+
+bazel run @test_infra//robots/pr-creator -- \
+    --github-token-path="${token}" \
+    --org munnerz --repo testing --branch autobump-images \
+    --title="${title}" --match-title="Bump ${BUILD_DIR} jobs" \
+    --body="Automatically bumped jobs that referenced image ${BUILD_DIR}" \
+    --source="${user}":autobump-"${BUILD_DIR}" \
+    --confirm
+
+echo "Complete!"
